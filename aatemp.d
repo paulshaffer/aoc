e1ecba7fb54ec42b2ad1034f315ff1941ebf38dc
Paul <phshaffer@gmail.com>
2023-03-13 13:36:14 -0400

    Challenges up through 12b.

--- aatemp.d
+++ aatemp.d
@@ -1,25 +1,30 @@
-﻿
-import std.stdio;
+﻿import std.stdio : writeln;
+import std.algorithm : map;
+import std.range : iota;
+import std.parallelism : taskPool;
+import core.time: MonoTime;
 
-int isPrime(int n){
-	  int i, count;
+/* On Intel i7-3930X and gdc 9.3.0:
+ * 5140ms using std.algorithm.reduce
+ * 888ms using std.parallelism.taskPool.reduce
+ *
+ * On AMD Threadripper 2950X, and gdc 9.3.0:
+ * 2864ms using std.algorithm.reduce
+ * 95ms using std.parallelism.taskPool.reduce
+ */
+void main()
 
-      for(i=1; i<=n; i++) {
-        if (n % i == 0) {
-          count = count + 1;
-        }
-      }
+{
+  auto progStartTime = MonoTime.currTime;
 
-     if (count == 2) {
-        return 1;
-      } else {
-        return 0;
-      }
+  auto nums = iota(1.0, 1_000_000_000.0);
+
+  auto x = taskPool.reduce!"a + b"(
+      0.0, map!"1.0 / (a * a)"(nums)
+  );
+
+  writeln("Sum: ", x);
+
+  auto progEndTime = MonoTime.currTime;
+writeln(progEndTime - progStartTime);
 }
-void main(){
-writeln(isPrime(2));
-writeln(isPrime(33));
-writeln(isPrime(37));
-writeln(isPrime(7879));
-}
-
