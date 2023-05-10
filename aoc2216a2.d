5b5b069e6f8873e8b62b782aacacc14a67e089cd
Paul <phshaffer@gmail.com>
2023-05-09 22:08:53 -0400

    oac2216a2

--- aoc2216a2.d
+++ aoc2216a2.d
@@ -1,13 +1,15 @@
-﻿module aoc2216a;
+﻿module aoc2216a2;
 
 import std.stdio;
 import std.file;
 import std.conv: to;
 import std.string;
 import std.algorithm: sort;
+import std.range: iota, repeat;
 import std.array;
 import std.traits;
 import core.time: MonoTime;
+import std.typecons;
 
 File fo;
 File f;
@@ -16,13 +18,14 @@ Valve[ubyte] vlv;
 ubyte[string] vlvTextToUbyteLookup;
 string[ubyte] vlvUbyteToTextLookup;
 ubyte[ubyte] unqRateToVlv;
+ubyte[ubyte] zeroVlvs;
+ubyte[ubyte] flowVlvs;
+
 size_t rT; // total of all flow rates
 ubyte firstVlv = 1;
 ubyte idByte;
-Path currPath, bestPath;
-size_t pathCount;
-enum MAX_PATHS = 10_000_000;
-Path[MAX_PATHS] allPaths;
+uint[60][60] timeCost;
+
 
 // Timed main() vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 void main(string[] args) {
@@ -37,35 +40,30 @@ auto progStartTime = MonoTime.currTime;
 	// "Valve YK has flow rate=0; tunnels lead to valves GL, FT,..etc"
 	loadFileDataIntoValve_aa();
 	for(ubyte i=1; i <= (vlv.length); i++) fo.writefln("%2s %s %s",i,vlvUbyteToTextLookup[i],vlv[i]);
-	// fo.writeln(firstVlv);
-	// foreach(rate;unqRateToVlv.keys.sort) fo.writef("%s-%s  ",rate,unqRateToVlv[rate]);
-	writeln(rT);
-	// fo.writeln(unqRateToVlv.length);
+	writeln("Total of all valve rates: ",rT);
 
-	move(1, -1);
-
-	pathCount.writeln;
-
-	bool dupRate;
-	ubyte[25] rates;
-	for(size_t i=1; i<=pathCount; i++) {
-		rates = 0;
-		foreach(rate;allPaths[i].r) {
-			if((rates[rate]) && (rate > 0)) {
-				dupRate = true; break;
-			} else {
-				rates[rate] = 1;
-			}
-		}
-		fo.writeln(allPaths[i]);
-		if(dupRate) {
-			writeln("Duplicate Rate");
-			break;
-		}
+	if(allGraphEdgesBidirectional) {
+		writeln("All edges are bidirectional.  Graph is undirected.");
+	} else {
+		writeln("Some graph edges are unidirectional");
 	}
-	if(!dupRate) writeln("No duplicate rates.");
 
-	
+	fillTimeCostTable();
+
+	printTimeCostTable();
+
+	eliminateZeroRateValves();
+
+	fo.writeln("\n");
+	printTimeCostTable();
+
+	fo.writeln("\n");
+	foreach(key;flowVlvs.keys.sort) if(flowVlvs[key]) fo.writef("%3s",key);
+	fo.writeln();
+	foreach(key;zeroVlvs.keys.sort) if(zeroVlvs[key]) fo.writef("%3s",key);
+	fo.writeln();
+
+
 //-----------------------------------------------------------------------------
 auto progEndTime = MonoTime.currTime;
 writeln(progEndTime - progStartTime);
@@ -75,40 +73,7 @@ writeln(progEndTime - progStartTime);
 struct Valve {
 	ubyte rate = 0;
 	ubyte[] nextVlvs;
-	ubyte nextVlv = 0;
-	byte i = -1;
-	byte indexAtTopLevel;
-	bool lastVlvOption = false;
 	bool visited = false;
-
-	void nextVlvOption() {
-			i++;
-			i = i >= nextVlvs.length ? 0 : i;
-			nextVlv = nextVlvs[i];
-		lastVlvOption = (i + 1 >= nextVlvs.length) ? true : false;
-	}
-}
-
-struct Path {
-	ubyte[30] p;
-	ubyte[30] r;
-	ushort[30] subt;
-	byte totalRate;
-	ushort total;
-	string toString() const {
-		string strP=" ",strR=" ",strS;
-		foreach(size_t i, id; p) {
-			if(id == 0) break;
-			//fo.writeln(id);
-			//fo.writeln(vlvUbyteToTextLookup[id]);
-			strP = strP ~ vlvUbyteToTextLookup[id] ~ "  ";
-			strR = strR ~ format("%2s  ",r[i]);
-			strS = strS ~ format("%3s ",subt[i]);
-		}
-		string s1 = strP~"\n"~strR;//~"\n"~strS~"\n";
-		// string s2 = format("total rate: %4s   total flow: %4s \n",totalRate,total);
-		return s1; //~ s2;
-	}
 }
 
 void loadFileDataIntoValve_aa() {
@@ -160,44 +125,72 @@ void loadFileDataIntoValve_aa() {
 		 }
 	}
 }
-// call function 1st time with v=1, time = -1
-void move(ubyte v, byte time) {
-	time++;
-	currPath.p[time] = v;
-	currPath.r[time] = vlv[v].visited ? 0 : vlv[v].rate;
-	Valve saveValveState = vlv[v];
 
-	if(time < 28) {
-		if(!vlv[v].visited) {
-			vlv[v].visited = true;
-			while(!vlv[v].lastVlvOption) {
-				vlv[v].nextVlvOption();
-				vlv[v].indexAtTopLevel = vlv[v].i;
-				move(vlv[v].nextVlv, time);
-			}
-		} else { // vlv[v].visited && time < 28
-			vlv[v].nextVlvOption();
-			if(vlv[v].i != vlv[v].indexAtTopLevel) {
-				while(vlv[v].i != vlv[v].indexAtTopLevel) {
-					move(vlv[v].nextVlv, time);
-					vlv[v].nextVlvOption();
+bool allGraphEdgesBidirectional() {
+	bool bidirectional = false;
+	for(ubyte currVlv=1; currVlv <= vlv.length; currVlv++) {
+		foreach(nextVlv;vlv[currVlv].nextVlvs) {
+			foreach(thisVlv;vlv[nextVlv].nextVlvs) {
+				if(thisVlv == currVlv) {
+					bidirectional = true; break;
+				} else {
+					bidirectional = false;
 				}
-			} else {// vlv[v].i == vlv[v].indexAtTopLevel
-				pathCount++;
-				allPaths[pathCount] = currPath;
 			}
+			if(!bidirectional) break;
 		}
-	} else if(time == 28) {
-		pathCount++;
-		allPaths[pathCount] = currPath;
-	} else { //time > 28
-		fo.writeln("Error.  Time > 28 minutes: time: ",time,"\n",currPath);
+		if(!bidirectional) break;
 	}
-	currPath.p[time] = 0;
-	currPath.r[time] = 0;
-	vlv[v] = saveValveState;
+	return bidirectional;
 }
 
-void optimizedCurrPath() {
-	
+void fillTimeCostTable() {
+	for(ubyte fromVlv=1; fromVlv <= vlv.length; fromVlv++) {
+		foreach(toVlv;vlv[fromVlv].nextVlvs) {
+			timeCost[fromVlv][toVlv] = 1;
+			//timeCost[toVlv][fromVlv] = 1;
+		}
+	}
+}
+
+void printTimeCostTable() {
+	string header = "   ";
+	foreach(num;iota(1,59,1)) header ~= format("%3s",num);
+	fo.writeln(header);
+	fo.writeln(repeat('-',177));
+	for(ubyte fromVlv=1; fromVlv <= vlv.length; fromVlv++) {
+		fo.writef("%2s>",fromVlv);
+		for(ubyte toVlv=1; toVlv <= vlv.length; toVlv++) {
+			if((toVlv%10 ==1) && (toVlv > 10)) {
+				fo.writef(":%2s",timeCost[fromVlv][toVlv]);
+			} else {
+				fo.writef("%3s",timeCost[fromVlv][toVlv]);
+			}
+		}
+		fo.writefln(" <%2s",fromVlv);
+		if(fromVlv%10 == 0) fo.writeln(repeat('-',177));
+	}
+	fo.writeln(repeat('-',177));
+	fo.writeln(header);
+}
+
+void eliminateZeroRateValves() {
+	for(ubyte zeroVlv=1; zeroVlv <= vlv.length; zeroVlv++) {
+		if(vlv[zeroVlv].rate > 0) {
+			flowVlvs[zeroVlv] = 1;
+			continue;
+		}
+
+		zeroVlvs[zeroVlv] = 1;
+		for(ubyte tableRow =1; tableRow <= vlv.length; tableRow++) {
+			if(tableRow == zeroVlv) continue;
+			if(timeCost[tableRow][zeroVlv] == 0) continue;
+
+			foreach(toVlv;vlv[zeroVlv].nextVlvs) {
+				timeCost[tableRow][toVlv] = timeCost[tableRow][zeroVlv] + 1;
+			}
+			// eliminate table reference to valve
+			timeCost[tableRow][zeroVlv] = 0;
+		}
+	}
 }
