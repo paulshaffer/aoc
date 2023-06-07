bbe393f67f9b11016e7bf28adca61322586a7023
Paul <phshaffer@gmail.com>
2023-06-07 19:00:21 -0400

    aoc2217a2 updates

--- aatemp.d
+++ aatemp.d
@@ -7,13 +7,22 @@ import std.file;
 import core.time: MonoTime;
 import std.conv;
 
+enum Result{move, ignore, end}
 
 // Timed main() vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 void main(string[] args) {
 auto progStartTime = MonoTime.currTime;
 //-----------------------------------------------------------------------------
+writeln(compareChars("#", "."));
+writeln(compareChars("#", "@"));
+writeln(compareChars("#", "#"));
+writeln(compareChars(".", "."));
+writeln(compareChars(".", "@"));
+writeln(compareChars(".", "#"));
+writeln(compareChars("@", "."));
+writeln(compareChars("@", "@"));
+writeln(compareChars("@", "#"));
 
-char[][] c;
 
 
 
@@ -22,3 +31,15 @@ auto progEndTime = MonoTime.currTime;
 writeln(progEndTime - progStartTime);
 }
 // Timed main() ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Result compareChars(string fromS, string toS) {
+	Result result = Result.ignore;
+	if(fromS == "@") {
+		if(toS == "." || toS == "@") {
+			result = Result.move;
+		} else { // toS = "#" or "|"
+			result = Result.end;
+		}
+	}	// else fromS = "." or "#", result.ignore
+	return result;
+}
--- aoc2217a2.d
+++ aoc2217a2.d
@@ -1,12 +1,12 @@
 ﻿module aoc2217a2;
 
 import std.algorithm;
-import std.regex;
 import std.stdio;
 import std.file;
 import core.time: MonoTime;
 import std.conv;
 import std.string: strip;
+import std.array: replace;
 
 File f,fo,f2;
 
@@ -15,17 +15,23 @@ enum numRocks = 2022;
 enum tallestRock = 4;
 enum chamberWidth = 9;
 enum chamberHeight = tallestRock * numRocks;
-
-struct Rock {
-	char[][] yx; //row[] col[]
-	int h, w; // height, width
-}
-Rock[] rocks;
+enum Result {move, ignore, end}
 
 char[chamberWidth][chamberHeight] chamber;
 string jets;
-int jetIndex;
+ulong jetIndex;
 
+struct Rock {
+	char[][] yx; //row[] col[]
+	ulong h, w; // height, width
+}
+Rock rock;
+Rock[] rocks;
+
+struct RockMarker {
+	ulong top, bottom, left, right;
+}
+RockMarker rockMarker;
 
 
 // Timed main() vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
@@ -38,32 +44,25 @@ auto progStartTime = MonoTime.currTime;
 	fo = File("tempout.data","w");
 
 	loadRocks();
-	for(size_t rk; rk < rocks.length; rk++) {
-		fo.writeln(rocks[rk].length);
-		for(size_t rkl; rkl < rocks[rk].length; rkl++) {
-			fo.writeln(rocks[rk][rkl]);
-		}
-	}
+	rockShapeReplace("#","@");
+	// printRocks();
 
 	buildChamber();
 
 	jets = f2.readln.strip();
 
-	int rk = -1;
-	int rockFalls = 1;
-	int stackHeight = 0, prevStackHeight = 0, rkLwrLeft = 0;
-	while(rockFalls <= numRocks) {
+	int rk = -1, rockFalls = 1, stackHeight = 0, prevStackHeight = 0;
+	while(rockFalls <= 5 ) { //numRocks
 		rk++; rk = rk < rocks.length ? rk : 0;
 		rock = rocks[rk];
-		rkLwrLeft = insertRock(stackHeight, rock);
+		rockMarker = insertRock(stackHeight, rock); fo.write(rockMarker," ");
 		while(prevStackHeight != stackHeight) { // falling rock
 			jetEffect();
 
 			// if the stack height is unchanged after a rock fall event, the rock has settled
 			prevStackHeight = stackHeight;
-			fallEffect();
+			// fallEffect();
 		}
- 
 		rockFalls++;
 	}
 printChamber(0, stackHeight + 5);
@@ -84,6 +83,23 @@ void loadRocks() {
 			rocks[$-1].yx ~= s.dup;
 			s = f.readln.strip;
 		}
+		rocks[$-1].h = rocks[$-1].yx   .length;
+		rocks[$-1].w = rocks[$-1].yx[0].length;
+	}
+}
+
+void rockShapeReplace(string searchStr, string replaceStr) {
+	foreach(ref rk;rocks)
+		foreach(ref row;rk.yx)
+			row = replace(row, searchStr, replaceStr);
+}
+
+void printRocks() {
+	foreach(rk; rocks) {
+		fo.writeln(rk.h);
+		foreach(rkl; rk.yx) {
+			fo.writeln(rkl," ",rk.w);
+		}
 	}
 }
 
@@ -96,18 +112,51 @@ void buildChamber() {
 
 void printChamber(int low, int high) {
 	for(int hgt = high; hgt >= low; hgt--) {
-		fo.writeln(chamber[hgt]);
+		fo.writefln("%4s %s",hgt,chamber[hgt]);
 	}
 }
 
-int insertRock(ref int stackHeight, char[][] rock) {
-	for(int i = 0; i < rock.length; i++) {
-		chamber[stackHeight + rockDropSpacing + 1 + i][3..(3 + rock[0].length)] = to!string(rock[$-1-i]);
+RockMarker insertRock(ref int stackHeight, Rock rock) {
+	for(int i = 0; i < rock.h; i++) {
+		chamber[stackHeight + rockDropSpacing + 1 + i][3..(3 + rock.w)] = to!string(rock.yx[$-1-i]);
 	}
-	stackHeight += rock.length + rockDropSpacing;
-	return 
+
+	RockMarker marker = {
+		top		: stackHeight + rockDropSpacing + rock.h,
+		bottom	: stackHeight + rockDropSpacing + 1, 
+		left	: 3,
+		right	: 2 + rock.w };
+
+	stackHeight += rockDropSpacing + rock.h;
+	return marker;
 }
 
 void jetEffect() {
 	int jet = jets[jetIndex] == '>' ? 1 : -1;
+	fo.writefln("jet %2s jetIndex %5s",jet,jetIndex);
+	jetIndex++; jetIndex = jetIndex < jets.length ? jetIndex : 0;
+	char[9][] restore = chamber[rockMarker.bottom .. rockMarker.top + 1];
+
+	Result result;
+	for(ulong y = rockMarker.bottom; y <= rockMarker.top; y++) {
+		for(ulong x = rockMarker.left; x <= rockMarker.right; x++) {
+		}
+	if(result = Result.end) break;
+	}
+
+	if(result = Result.end) {
+		chamber[rockMarker.bottom .. rockMarker.top + 1] = restore;
+	}
+}
+
+Result compareChars(string fromS, string toS) {
+	Result result = Result.ignore;
+	if(fromS == "@") {
+		if(toS == "." || toS == "@") {
+			result = Result.move;
+		} else { // toS = "#" or "|"
+			result = Result.end;
+		}
+	}	// else fromS = "." or "#", result.ignore
+	return result;
 }
